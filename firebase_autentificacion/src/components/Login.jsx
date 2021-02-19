f2c87141c7a345365f78211cb3781c320cd40cc3
Diegolopez-ing <diegolopez.coder@gmail.com>
2021-02-18 21:45:31 -0500

    Estructuracion de Incio

--- firebase_autentificacion/src/components/Login.jsx
+++ firebase_autentificacion/src/components/Login.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Login = () => {
     return (
-        <div>
-            <h2>Login</h2>
+        <div className="row mt-5">
+            <div className="col"></div>
+            <div className="col">
+                <input className='form-control mt-4' placeholder='Introduce el Email' type="text"/>
+                <input className='form-control mt-4' placeholder='Introduce la Password' type="password"/>
+               <button className='btn btn-dark btn-block mt-4' type="submit" value='Registrar Usuarios'>Registrar Usuarios</button>
+            </div>
+            <div className="col"></div>
         </div>
     )
 }

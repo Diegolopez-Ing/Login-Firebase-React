1faa2631d2035e389c1bacc83e6b04472dee0728
Diegolopez-ing <diegolopez.coder@gmail.com>
2021-02-19 23:31:25 -0500

    COmprobar usuarion n y cerrar sesion

--- firebase_autentificacion/src/components/Menu.jsx
+++ firebase_autentificacion/src/components/Menu.jsx
@@ -1,20 +1,47 @@
-import React from 'react'
+import React,{useEffect,useState}from 'react'
 import {Link} from 'react-router-dom'
+import {auth} from '../firebaseConfig'
+
+
 
 
 const Menu = () => {
+    const [usuario, setusuario] = useState(null)
+
+    useEffect(() => {
+        auth.onAuthStateChanged((user)=>{
+            if(user){
+                setusuario(user.email)
+            }
+        })
+        
+    }, [])
+
+    const CerrarSesion=()=>{
+        auth.signOut()
+        setusuario(null)
+    }
+
     return (
         <div>
            <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
                <ul className='navbar-nav mr-auto'>
                    <li className='nav-item'><Link to='/'>Inicio</Link></li>
-               </ul>
-               <ul>
                    <li><Link className='nav-link' to='/login'>Login</Link></li>
-               </ul>
-               <ul>
                    <li><Link className='nav-link' to='/admin'>Admin</Link></li>
                </ul>
+               {
+                   usuario ? 
+                   (
+                      <button
+                      onClick={CerrarSesion}
+                       className='btn btn-danger'>Cerrar Sesion</button> 
+                   )
+                   :
+                   (
+                       <span></span>
+                   )
+               }
            </nav>
         </div>
     )

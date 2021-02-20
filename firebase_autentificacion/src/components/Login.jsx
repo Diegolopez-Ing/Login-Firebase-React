66f49ba5703cd178af2c4c3517a1d163a8e9a754
Diegolopez-ing <diegolopez.coder@gmail.com>
2021-02-19 22:16:28 -0500

    COnfiguracion de incio y conexion a Firebase

--- firebase_autentificacion/src/components/Login.jsx
+++ firebase_autentificacion/src/components/Login.jsx
@@ -1,14 +1,44 @@
-import React from 'react'
+import React , {useState} from 'react'
+import {auth} from '../firebaseConfig'
 
 const Login = () => {
+    const [email, setemail] = useState('')
+    const [pass, setpass] = useState('')
+
+    const RegistrarUsuario=(e)=>{
+        e.preventDefault()
+        try {
+            auth.createUserWithEmailAndPassword(email,pass)
+            alert('Usuario registrado')
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <div className="row mt-5">
             <div className="col"></div>
             <div className="col">
-                <input className='form-control mt-4' placeholder='Introduce el Email' type="text"/>
-                <input className='form-control mt-4' placeholder='Introduce la Password' type="password"/>
-               <button className='btn btn-dark btn-block mt-4' type="submit" value='Registrar Usuarios'>Registrar Usuarios</button>
-            </div>
+                <form onSubmit={RegistrarUsuario} className='form-group'>
+                    <input 
+                        onChange={(e)=>{setemail(e.target.value)}}
+                        className='form-control mt-4' 
+                        placeholder='Introduce el Email' 
+                        type="text"
+                    />
+
+                    <input 
+                        onChange={(e)=>{setpass(e.target.value)}}
+                        className='form-control mt-4' 
+                        placeholder='Introduce la Password' 
+                        type="password"
+                    />
+                <input 
+                    className='btn btn-dark btn-block mt-4' 
+                    type="submit" 
+                    value='Registrar Usuarios' value='Registrar Usuarios'></input>
+                 </form>
+                </div>         
             <div className="col"></div>
         </div>
     )
